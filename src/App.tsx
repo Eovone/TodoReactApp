4b22df2cf96023e333b300079a572072e336a2b5
Eovone <114399255+Eovone@users.noreply.github.com>
2023-09-17 12:23:29 +0200

    tailwind installed and first component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import CreateTodo from './Components/CreateTodo';
 
 function App() {
   return (
     <div className="App">
-      <div><p>hej</p></div>
+      <CreateTodo />
     </div>
   );
 }
---
+++ src/Components/CreateTodo.tsx
@@ -0,0 +1,16 @@
+import React, { FC, } from 'react';
+import { TodoModel } from '../Models/TodoModel';
+
+interface CreateTodoProps { 
+
+}
+  
+const CreateTodo: FC<CreateTodoProps> = (props) => {
+      
+  
+return (
+    <div>hej</div>
+);
+}
+  
+export default CreateTodo;
